7ced880bc20667c907840081e9ac56c01dcb2624
lucasvieites <lucas@codexion.com>
2012-09-03 11:40:03 +0200

    Added comments

--- codexion/src/com/codexion/utilities/WordCounter.java
+++ codexion/src/com/codexion/utilities/WordCounter.java
@@ -1,3 +1,9 @@
+/**
+ * WordCounter.java - a little program to write 100 word texts, also known as
+ * "drabbles"
+ * @author Lucas Vieites
+ * @version 1.0
+ */
 package com.codexion.utilities;
 
 import javax.swing.*;
@@ -6,24 +12,45 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class WordCounter extends JFrame {
+	/**
+	 * Set a random version number
+	 */
 	private static final long serialVersionUID = 8819410241430015289L;
 
+	/**
+	 * A container to put all the UI elements in
+	 */
 	private Container contentPane;
 
+	/**
+	 * A customised JTextArea
+	 */
 	private CustomJTextarea textArea;
 
+	/**
+	 * A textfield to put messages in, especially the current word count
+	 */
 	private JTextField reportingField;
 
-	/** Creates a new instance of WordCounter */
+	/**
+	 * Creates a new instance of WordCounter
+	 */
 	public WordCounter() {
+		// Set the size of the JFrame
 		setPreferredSize(new Dimension(400, 400));
+		// We don't want it to become larger that this
 		setMaximumSize(new Dimension(800, 800));
+		// Set a title for the window
 		setTitle("Wordcounter 100");
 		// Put the window on the center of the screen
-		Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-		this.setBounds((screenSize.width-400)/2, (screenSize.height-400)/2, 400, 400);
+		Dimension screenSize = java.awt.Toolkit.getDefaultToolkit()
+				.getScreenSize();
+		this.setBounds((screenSize.width - 400) / 2,
+				(screenSize.height - 400) / 2, 400, 400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
+		// Create the area we'll write in and tweak it a little bit to our
+		// liking
 		textArea = new CustomJTextarea();
 		// Set max word count to 100
 		textArea.setMaxWordCount(100);
@@ -31,14 +58,19 @@ public class WordCounter extends JFrame {
 		textArea.setEditable(true);
 		textArea.addKeyListener(new KeyListener() {
 			public void keyTyped(KeyEvent evt) {
+				// Check if we have typed too many words
 				if (textArea.isOverMaxWordCount()) {
-					//textArea.removeLastWord();
+					// Remove the last word so as to not exceed the max number
+					// Commented out as it's not intuitive
+					// textArea.removeLastWord();
 					// Set color to red
-					reportingField.setForeground(new Color(255,0,0));
+					reportingField.setForeground(new Color(255, 0, 0));
 				} else {
+					// We're back under the max number
 					// Set text color back to black
-					reportingField.setForeground(new Color(0,0,0));
+					reportingField.setForeground(new Color(0, 0, 0));
 				}
+				// Inform of the current number of words
 				reportingField.setText("Words entered: "
 						+ textArea.getCurrentWordCount());
 			}
@@ -47,8 +79,7 @@ public class WordCounter extends JFrame {
 			}
 
 			public void keyPressed(KeyEvent arg0) {
-				// TODO Auto-generated method stub
-				
+
 			}
 		});
 		reportingField = new JTextField();
@@ -60,10 +91,11 @@ public class WordCounter extends JFrame {
 	}
 
 	/**
+	 * Create a JFrame with the custom textarea in which we will write
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Call the constructor
 		new WordCounter();
 	}
 }
